0505237306b91b0b6050bfce2f5a6ca16f916a1e
Denis Borysenko <dborysenko.ed@gmail.com>
2024-02-16 15:44:14 +0200

    removed limit

--- mygame/main.py
+++ mygame/main.py
@@ -9,7 +9,6 @@ COLOR_WHITE = (255,255,255)
 COLOR_BLACK = (0,0,0)
 PLAYER_WIDTH = 20
 PLAYER_HEIGHT = 20
-LIMIT = 0
 
 # -- GAME SETTINGS
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -24,42 +23,36 @@ pygame.init()
 
 playing = True
 
-
 while playing:  
 
     FPS.tick(120)
-    if LIMIT != 5: 
-        for event in pygame.event.get(): 
-            if event.type == QUIT: 
-                palying = False
 
-        main_display.fill(COLOR_BLACK)
+    for event in pygame.event.get(): 
+        if event.type == QUIT: 
+            palying = False
 
-        if player_rect.bottom >= HEIGHT:
-            player_speed[1] = -player_speed[1]
-            touches += 1
-        if player_rect.right >= WIDTH:
-            player_speed[0] = -player_speed[0]
-            touches += 1
-        if player_rect.top < 0:
-            player_speed[1] = -player_speed[1]
-            touches += 1
-        if player_rect.left < 0:
-            player_speed[0] = -player_speed[0]
-            touches += 1
-        #print(player_rect.bottom)
+    main_display.fill(COLOR_BLACK)
 
-        main_display.blit(player, player_rect)
+    if player_rect.bottom >= HEIGHT:
+        player_speed[1] = -player_speed[1]
 
-        player_rect = player_rect.move(player_speed)
+    if player_rect.right >= WIDTH:
+        player_speed[0] = -player_speed[0]
 
-        pygame.display.flip()
-        print("Touches:",touches)
-    else: 
-        pygame.display.quit()
-        pygame.quit()
-        sys.exit()
-        exit()
-        
+    if player_rect.top < 0:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.left < 0:
+        player_speed[0] = -player_speed[0]
+
+    main_display.blit(player, player_rect)
+
+    player_rect = player_rect.move(player_speed)
+
+    pygame.display.flip()
 
 
+pygame.display.quit()
+pygame.quit()
+sys.exit()
+exit()
