2ab26bb64af8d7ba347f436b77ead960252ab164
Denis Borysenko <dborysenko.ed@gmail.com>
2024-02-18 16:47:58 +0200

    added score

--- mygame/main.py
+++ mygame/main.py
@@ -1,6 +1,6 @@
 import pygame, sys, random
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT, K_0
-
+from pygame.locals import *
 # -- GAME CONSTANTS
 WIDTH = 800
 HEIGHT = 600
@@ -9,6 +9,7 @@ COLOR_PLAYER = (255, 255, 255) # WHITE
 COLOR_GAMEFIELD = (0, 0, 0) # BLACK
 COLOR_ENEMY = (255, 0, 0) # RED
 COLOR_BONUS = (250, 215, 0) # GOLDEN
+COLOR_SCORE = (0, 255, 0)
 PLAYER_WIDTH = 20
 PLAYER_HEIGHT = 20
 ENEMY_WIDTH = 30
@@ -17,6 +18,8 @@ BONUS_WIDTH = 15
 BONUS_HEIGHT = 15
 CREATE_ENEMY = pygame.USEREVENT + 1
 CREATE_BONUS = pygame.USEREVENT + 2
+#FONT = pygame.font.SysFont('C:\Windows\Fonts\Arial.ttf', 20)
+#FONT = pygame.font.SysFont('Arial', 20)
 
 # -- GAME SETTINGS
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -53,11 +56,15 @@ def create_bonus():
 
 # -- GAME PROCESS
 pygame.init()
+pygame.font.init()
+
+FONT = pygame.font.SysFont('Arial', 20)
 
 playing = True
 
 enemies = []
 bonuses = []
+score = 0
 
 while playing:  
 
@@ -76,29 +83,37 @@ while playing:
     keys = pygame.key.get_pressed()
 
     if keys[K_DOWN] and player_rect.bottom < HEIGHT : 
-        print("DOWN")
+        #print("DOWN")
         player_rect = player_rect.move([0, 1])
 
     if keys[K_UP] and player_rect.top > 0 : 
-        print("UP")
+        #print("UP")
         player_rect = player_rect.move([0, -1])
 
     if keys[K_LEFT] and player_rect.left > 0 : 
-        print("LEFT")
+        #print("LEFT")
         player_rect = player_rect.move([-1, 0])
 
     if keys[K_RIGHT] and player_rect.right < WIDTH : 
-        print("RIGHT")
+        #print("RIGHT")
         player_rect = player_rect.move([1, 0])
-
+        
     for enemy in enemies: 
         enemy[1] = enemy[1].move(enemy[2]) #enemy_rect
         main_display.blit(enemy[0], enemy[1])
+        if player_rect.colliderect(enemy[1]):
+            print("boom")
+            playing = False
 
     for bonus in bonuses: 
         bonus[1] = bonus[1].move(bonus[2]) #bonus_rect
         main_display.blit(bonus[0], bonus[1])
- 
+        if player_rect.colliderect(bonus[1]):
+            print("bonus")
+            bonuses.pop(bonuses.index(bonus))
+
+
+    main_display.blit(FONT.render(str(score), True, COLOR_SCORE), (WIDTH-50, 20))
     main_display.blit(player, player_rect)
 
     pygame.display.flip()
@@ -106,12 +121,13 @@ while playing:
     for enemy in enemies: 
         if enemy[1].left < 0:
             enemies.pop(enemies.index(enemy))
-
+        
     for bonus in bonuses: 
         if bonus[1].bottom > HEIGHT:
             bonuses.pop(bonuses.index(bonus))
+        
 
-    print(f"ENEMIES:\t{len(enemies)}\nBONUSES:\t{len(bonuses)}")
+    #print(f"ENEMIES:\t{len(enemies)}\nBONUSES:\t{len(bonuses)}")
     
 # -- EXIT WHEN PRESSED 0
     if keys[K_0]: 
